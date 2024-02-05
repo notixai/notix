469adc96cf6ba4d87356dfaa01d4fd8cccce1277
PeripheralSheep <mikkel.boxill@gmail>
2024-02-05 19:42:39 -0400

    Create Upload Page (no css)

--- frontend/client/src/components/UploadPage/UploadPage.jsx
+++ frontend/client/src/components/UploadPage/UploadPage.jsx
@@ -3,45 +3,81 @@ import {useState} from 'react'
  * 
  * Upload page for the app
  */
-export default function UploadPage(){
-    const [tags,setTags] = useState(["CSS","HTML"]);
 
+let curID = 1;
+
+export default function UploadPage(){
+    const [tags,setTags] = useState([]);
+    const [newTag, setNewTag] = useState('')
+    const [curFile, setCurFile] = useState({});
     /**
      * 
      * @param {event} e 
      * Takes the input from the label-input and appends it to the other tags
      */
-    function addTag(e){
-        if(e.key==="Enter")
+    function handleAddTag(e){
+        if((e.type === 'keydown' && e.key === 'Enter') || e.type==='blur')
         {
-            setTags( (prevTags) => [...prevTags,e.target.value]);
-            e.target.value = "";
+            if(newTag.trim())
+            {
+                setTags([...tags,{id: curID++, tagName:newTag}]);
+                setNewTag('');
+            }        
         }
     }
+    function handleAddFile(e){
+        setCurFile(e.target.files[0])
+    }
+
     return(
         <>
-            <form className="form upload-form" action="" method="POST" onSubmit={(e) => e.preventDefault()}>
-                <div className="tags-container">
-                    <ul id="tag-list">
-                        {
-                            tags.map( (tag,index) => <li key={index}>{tag}</li>)
-                        }
-                    </ul>
-                    <input id="tag-input" type="text" placeholder="Add Tag" onKeyUp={addTag}/>
-                </div>
-                
-                <label id="audio-upload-label" htmlFor="audio-upload">Upload Audio</label>
+            <div className="tags-container">
+                <ul id="tag-list">
+                    {
+                        tags.map( (tag) => (
+                            <li key={tag.id}>
+                                {tag.tagName}
+                                <button onClick={() => {
+                                    setTags( (curTags) => curTags.filter( (curTag) => tag.id != curTag.id))
+                                }}>
+                                    &times;
+                                </button>
+                            </li>
+                        )
+                        )
+                    }
+                </ul>
                 <input 
+                    id="tag-input" 
+                    type="text" 
+                    placeholder="Add Tag" 
+                    value={newTag}
+                    onBlur={handleAddTag} 
+                    onKeyDown={handleAddTag}
+                    onChange={ (e) => {setNewTag(e.target.value)}}
+                />
+            </div>
+            <form className="form upload-form" action="" method="POST" onSubmit={(e) => e.preventDefault()}>
+                <label id="audio-upload-label" htmlFor="audio-upload">Upload Audio</label>
+                <input
                     type="file" 
                     id="audio-upload" 
                     name="audio-upload"
                     accept=".mp3,.wav"
-                    placeholder="Upload Audio" />
-                <div className="file-info">
-                    <span>Name: </span>
-                    <span>Size: </span>
-                    <span>Length: </span>
-                </div>
+                    placeholder="Upload Audio"
+                    onChange={handleAddFile}
+                    required
+                />
+                
+                {curFile &&
+                (
+                    <div className="file-info">
+                        <span>Name: {curFile.name}</span>
+                        <span>Size: {curFile.size}</span>
+                        <span>Length: {curFile.duration}</span>
+                    </div>
+                )}
+                
             </form>
         </>
     )
