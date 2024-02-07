8ee39e4a6253ab92edc5e3befc0559d8db4f0477
Thelema <thelemagrannum@gmail.com>
2024-02-07 17:20:16 -0400

    Added summarisation

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, make_response, request
 import requests , time, json
 from dotenv import load_dotenv
 import assemblyai as aai
+import openai
 import os
 
 load_dotenv()
@@ -59,15 +60,31 @@ def transcript():
     response.mimetype = "text/plain"
     return response 
   except Exception as error:
-    return jsonify({error: "Transcription error"}), 500
+    return jsonify({"Transcription error": error}), 500
 
 @app.route('/summarise', methods=['POST'])
 def summarise():
   try:
-      # TODO: add summarisation AI code
-    return None
+      openai.api_key = os.environ('OPENAI_API_KEY')
+
+      completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+
+            # TODO:  Add meeting minutes
+          {"role": "user", "content":  """I will input the transcript for a YouTube video discussing ChatGPT. Correct any errors you may find in the transcript. Provide a bullet point summary of these notes as a good student would for their class.
+          Here's the transcript: {}
+        """.format(request.body)},
+
+      ]
+    )
+      res = make_response(completion.choices[0].messages['content'], 200)
+      res.mimetype = "text/plain"
+  
+      return res
+  
   except Exception as error:
-    return jsonify({error: "Summarisation error"}), 500
+    return jsonify({"Summarisation error": error}), 500
 
 if __name__ == '__main__':
   app.run(debug=True, host='127.0.0.1')
