db6f2ee8b6317657369da172be58b5d177adacce
PeripheralSheep <mikkel.boxill@gmail>
2024-02-08 01:32:41 -0400

    other components

---
+++ frontend/client/src/components/General/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <header className="notix-header">
+            <img src="" alt="" className="notix-logo" />
+            <span className="notix-name">Notix</span>
+            <div className="user">
+                <img src="" alt="" className="user-icon" />
+            </div>
+        </header>
+    )
+}
---
+++ frontend/client/src/components/General/NavMenu.jsx
@@ -0,0 +1,17 @@
+function NavMenu(){
+    return(
+        <nav className="nav-menu">
+            <div className="navigation classroom-navigation">
+                <h2 className="navigation-label label">Classrooms</h2>
+                <ol className="user-classrooms"></ol>
+            </div>
+            <div className="navigation notebook-navigation">
+                <h2 className="label navigation-label notebook-label">
+                    Notebooks
+                </h2>
+                <ol className="user-notebooks"></ol>
+            </div>
+        </nav>
+    )
+    
+}
