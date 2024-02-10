e8b44ce1de9c0f348a79762f3a5cc9006cd3f0a5
PeripheralSheep <mikkel.boxill@gmail>
2024-02-10 12:06:38 -0400

    Styling

--- frontend/client/src/components/General/NavMenu.jsx
+++ frontend/client/src/components/General/NavMenu.jsx
@@ -1,15 +1,24 @@
 export default function NavMenu(){
     return(
         <nav className="nav-menu">
+            <h2>Notix</h2>
             <div className="navigation classroom-navigation">
-                <h2 className="navigation-label label">Classrooms</h2>
-                <ol className="user-classrooms"></ol>
+                <h2 className="navigation-label label">Classes</h2>
+                <ol className="user-classrooms">
+                    <li className="user-classroom">Dummy 1</li>
+                    <li className="user-classroom">Dummy 2</li>
+                    <li className="user-classroom">Dummy 3</li>
+                </ol>
             </div>
             <div className="navigation notebook-navigation">
                 <h2 className="label navigation-label notebook-label">
                     Notebooks
                 </h2>
-                <ol className="user-notebooks"></ol>
+                <ol className="user-notebooks">
+                    <li className="user-notebooks">Dummy 1</li>
+                    <li className="user-notebooks">Dummy 2</li>
+                    <li className="user-notebooks">Dummy 3</li>
+                </ol>
             </div>
         </nav>
     )
