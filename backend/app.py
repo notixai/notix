32cc6c5476e94b9deceda53266d18e12771dbd9b
Aquon Bovell <aquon_bovell@outlook.com>
2024-02-11 12:23:36 -0400

    Refactor transcription endpoint to use AssemblyAI Python requests

--- backend/app.py
+++ backend/app.py
@@ -1,65 +1,73 @@
 from flask import Flask, jsonify, make_response, request
-import requests , time, json
 from dotenv import load_dotenv
-import assemblyai as aai
-import openai
-import os
+import openai, os, requests, time
 
 load_dotenv()
 
 app = Flask(__name__)
 
-base_url = 'https://api.assemblyai.com/v2'
-
 headers = {
   'authorization': os.environ['ASSEMBLYAI_API_KEY'],
 }
 
+base_url = os.environ['ASSEMBLYAI_BASE_URL']
+
 @app.route('/transcribe', methods=['POST'])
 def transcript():
   try:
-    # Method 1 - Uses AssemblyAI Python requests
+    # TODO: check if the file is of type audio or video
+    file = request.files.get('media')
+    # check if the post request has the file part
+    if not file:
+      return jsonify({"error": "No audio or video file provided"}), 400
+    # read 1GB of the file
+    file_content = file.read(1000 * 1024 * 1024)
+    # upload the file to AssemblyAI
+    response = requests.post(base_url + 'upload', headers=headers, data=file_content)
+    # get the upload url
+    upload_url = response.json()['upload_url']
+    # configure the transcription
+    data = {
+      'audio_url': upload_url,
+      'speaker_labels': True,
+      'auto_highlights': True,
+      'word_boost': ["ChatGPT"],
+      'boost_param': 'high',
+      "auto_chapters": True,
+      "content_safety": True,
+      # "summarization": True,
+      # "summary_model": "informative",
+      # "summary_type": "bullets"
+    }
+    # start the transcription
+    transcript_url = requests.post(base_url + 'transcript', headers=headers, json=data)
+    # get the transcript id
+    transcript_id = transcript_url.json()['id']
+    # polling endpoint to check the status of the transcription
+    polling_endpoint = f"{base_url}transcript/{transcript_id}"
 
-    # file = request.files['file']
-    # file_content = file.read(5242880)
-    # response = requests.post(base_url + '/upload', headers=headers,data=file_content)
-    # upload_url = response.json()['upload_url']
-    # data ={
-    #   'audio_url': upload_url,
-    # }
-    # transcript_url = requests.post(base_url + '/transcript', headers=headers, json=data)
-    # transcript_id = transcript_url.json()['id']
-    # polling_endpoint = f"https://api.assemblyai.com/v2/transcript/{transcript_id}"
-
-    # while True:
-    #   response = requests.get(polling_endpoint, headers=headers).json()
-    #   status = response['status']
-    #   if status == 'completed':
-    #     print('Transcription is complete')
-    #     print(response['text'])
-    #     break
-    #   elif status == 'error':
-    #     raise RuntimeError(f"Transcription failed: {response['error']}")
-    #   else:
-    #     time.sleep(5)
-    # return response['text']
-
-    # Method 2 - Uses AssemblyAI Python SDK
-
-    file = request.files['file']
-    file.save(file.filename)
-    aai.settings.api_key = os.environ['ASSEMBLYAI_API_KEY']
-    config = aai.TranscriptionConfig(speaker_labels=True, auto_highlights=True)
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(file.filename, config=config)
-    result = ''
-    for utterance in transcript.utterances:
-      result += "Speaker " + str(utterance.speaker) + ": " + utterance.text + "\n"
-    print(result)
-    response = make_response(result, 200)
-    response.mimetype = "text/plain"
-    return response 
+    while True:
+      # poll the endpoint
+      response = requests.get(polling_endpoint, headers=headers).json()
+      # check the status of the transcription
+      status = response['status']
+      transcript = ''
+      # if the transcription is complete, break the loop
+      if status == 'completed':
+        print('Transcription is complete\n\n')
+        for utterance in response['utterances']:
+          transcript += f"Speaker {utterance['speaker']}: {utterance['text']}\n"
+        print(transcript)
+        break
+      # if the transcription failed, raise an error
+      elif status == 'error':
+        raise RuntimeError(f"Transcription failed: {response['error']}")
+      # else, wait for 3 seconds and poll the endpoint again
+      else:
+        time.sleep(3)
+    return jsonify({"transcript": transcript}), 200
   except Exception as error:
+    # return an error message
     return jsonify({"Transcription error": error}), 500
 
 @app.route('/summarise', methods=['POST'])
