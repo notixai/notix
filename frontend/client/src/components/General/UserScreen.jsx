5c90c3a903eca5cbc81364cc9ebdb0fc310fd661
TaigaTi <tarikabirch@gmail.com>
2024-02-11 15:52:46 -0400

    Added side navigation using MUI components

--- frontend/client/src/components/General/UserScreen.jsx
+++ frontend/client/src/components/General/UserScreen.jsx
@@ -2,6 +2,7 @@ import {Outlet} from 'react-router-dom';
 import Header from './Header.jsx'
 import NavMenu from './NavMenu.jsx'
 import {useNavigation} from 'react-router-dom'
+import SideNav from './SideNav.jsx'
 
 export default function UserScreen(){
     const navigation = useNavigation();
@@ -9,7 +10,8 @@ export default function UserScreen(){
         <div className="main-screen logged-in-screen">
             {navigation.state === "loading" && <h2>Waiting</h2>}
             <Header />
-            <NavMenu />
+            <SideNav />
+            {/* <NavMenu /> */}
             <main className="main-content">
                 <Outlet />
             </main>
