265108fe428c1fbe1bd85b909c72c7e6b6839297
Branden Spooner <bspooner@cimh.edu.bb>
2024-02-16 19:49:14 -0400

    Remove NavMenu header from UserScreen

--- frontend/client/src/components/General/UserScreen.jsx
+++ frontend/client/src/components/General/UserScreen.jsx
@@ -1,6 +1,5 @@
 import {Outlet} from 'react-router-dom';
 import Header from './Header.jsx'
-import NavMenu from './NavMenu.jsx'
 import {useNavigation} from 'react-router-dom'
 import SideNav from './SideNav.jsx'
 
