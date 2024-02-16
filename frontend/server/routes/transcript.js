20d412c137ac354aa9871ac647fe21b0ba311661
Branden Spooner <bspooner@cimh.edu.bb>
2024-02-16 05:27:32 -0400

    Accept editted transcript and get summary from AI. Summary stored in Class Collection.

--- frontend/server/routes/transcript.js
+++ frontend/server/routes/transcript.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require('axios');
 const transcriptRoutes = express.Router();
 
 const mongoose = require('mongoose');
@@ -33,6 +34,24 @@ transcriptRoutes.route("/transcripts/:classRoomId/:classId").get(async (req, res
     );
 });
 
+transcriptRoutes.route("/transcript").post(async (req, res) => {
+    // Add Editted Transcript to Class Document 
+    await Class.findOneAndUpdate({class_id: req.body.classID}, {editted_transcription: req.body.editted_transcript });
+    
+    const notix_class = await Class.findOne({class_id: req.body.classId});
+    const data = {topics: notix_class.tags, transcript: req.body.editted_transcript};
+    
+    let summary = await axios.post('http:/ai:12009/summarise', data)
+        .then((response) => {
+            return response.data.Summary;
+        })
+        .catch((error) => {
+            console.log(error);
+            return null;
+        });
+    
+    await Class.findOneAndUpdate({class_id: classID}, {summary: summary});
 
+});
 
 module.exports = transcriptRoutes;
