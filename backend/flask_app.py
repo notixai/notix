dcf3803b76fb78f4fc38c1f68c75e323dc24d311
PeripheralSheep <mikkel.boxill@gmail>
2024-02-16 17:55:19 -0400

    Fixes

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -99,4 +99,4 @@ def summarise():
     return jsonify({"Summarisation error": error}), 500
 
 if __name__ == '__main__':
-  app.run(debug=True, host='127.0.0.1', port=12009)
+  app.run(debug=True, host='0.0.0.0', port=12009)
