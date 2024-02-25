ff9c264e7731f5216b6191dcbbcbd929d3cd0718
Aquon Bovell <aquon_bovell@outlook.com>
2024-02-25 08:00:18 -0400

    Add Tailwind CSS configuration file and update Home component

--- frontend/client/src/components/General/Home.jsx
+++ frontend/client/src/components/General/Home.jsx
@@ -1,14 +1,13 @@
-import {Link} from 'react-router-dom'
-export default function Home(){
-    return (
-        <>
-            <h1>Welcome</h1>
-            <div className="links">
-                <Link to={'upload-audio'}>Upload Page</Link>
-                <Link to={'transcripts'}>View Transcripts</Link>
-                <Link to={'summaries'}>See Summaries</Link>
-            </div>
-        </>
-        
-    )
+import { Link } from "react-router-dom";
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <div className="links">
+        <Link to={"upload-audio"}>Upload Page</Link>
+        <Link to={"transcripts"}>View Transcripts</Link>
+        <Link to={"summaries"}>See Summaries</Link>
+      </div>
+    </>
+  );
 }
